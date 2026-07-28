a3edc595a7a1cb7442148c217c728f58e9f0abe3
Fercharicastillo <ferchari2000@gmail.com>
2026-07-28 14:39:38 -0500

    nuevos cambios a la ahoja de autores

---
+++ script/autores/autorespdf.js
@@ -0,0 +1,5 @@
+const BASE_PUBLICA = "https://fercharicastillo.github.io/chari";
+const RUTA_VISOR_PDF = "visor_pdfs/web/viewer.html?file=pdfs/";
+const RUTAS_AUTORES_PDF = "pdfs_autores/";
+
+// LA RUTA DEL PDF DEL CURRICUCULUM SE CONSTRUYE BASE_PUBLICA+RUTA_VISOR_PDF +RUTAS_AUTORES_PDF+categoriasLibros[archivo]
---
+++ script/autores/datosautores.js
@@ -0,0 +1,17 @@
+const categoriasLibros = [
+    {
+        id: "chari-fernando",
+        autor: "chari-fernando",
+        archivo: "chari-fernando.pdf"
+    },
+    {
+        id: "chari-erick",
+        autor: "chari-erick",
+        archivo: "documento-prueba.pdf"
+    },
+    {
+        id: "autor3",
+        autor: "autor3",
+        archivo: "documento-prueba.pdf"
+    }
+]
